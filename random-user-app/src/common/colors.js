d3001be6129f86e12fa8dadf9de1c603c559c468
damarendra <damarendran@gmail.com>
2019-03-02 12:11:02 +0700

    Add isLoading Selector

---
+++ random-user-app/src/common/colors.js
@@ -0,0 +1,5 @@
+export default {
+  white: '#fff',
+  dark_blue: '#1f618d',
+  light_blue: '#2980b9'
+};
--- random-user-app/src/ducks/selectors.js
+++ random-user-app/src/ducks/selectors.js
@@ -4,8 +4,14 @@ import {
 } from 'reselect';
 
 const usersSelector = state => state.users.users;
+const loadingSelector = state => state.users.loading;
 
 export const getUsers = createSelector(
   usersSelector,
   (users) => users
+);
+
+export const isLoading = createSelector(
+  loadingSelector,
+  (status) => status
 );
